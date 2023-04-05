63f341bf5379f234967299a575c9f6a328b18aae
Raafaye Faheem <spa45rky@gmail.com>
2023-04-05 17:02:36 +0500

    setup cra, installed jotai, tailwind and react-router. Setup folder structure and router

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import ServiceWorker from "./pages/service-worker";
+import JobPoster from "./pages/job-poster";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/service-worker",
+		element: <ServiceWorker />,
+	},
+	{
+		path: "/job-poster",
+		element: <JobPoster />,
+	},
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/service-worker.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+	return (
+		<div>
+			<h1>Service Worker</h1>
+		</div>
+	);
+};
